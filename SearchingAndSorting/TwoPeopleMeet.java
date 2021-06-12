9ea4c6470d606d88f53b26cc550204611748849c
GuchhaitPratyay <pratyay.guchait@gmail.com>
2021-06-12 21:56:37 +0530

    First Commit

---
+++ SearchingAndSorting/TwoPeopleMeet.java
@@ -0,0 +1,23 @@
+package SearchingAndSorting;
+
+import java.util.Scanner;
+
+public class TwoPeopleMeet {
+    private static boolean isMeet(int d1 , int v1 , int d2 , int v2) {
+
+    }
+
+    public static void main(String[] pg) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the Position of A: ");
+        int d1 = in.nextInt();
+        System.out.print("Enter the Speed of A: ");
+        int v1 = in.nextInt();
+
+        System.out.print("Enter the Position of B: ");
+        int d2 = in.nextInt();
+        System.out.print("Enter the Speed of B: ");
+        int v2 = in.nextInt();
+
+    }
+}
