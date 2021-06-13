060e18cca6cd111e8493c05a1f03d95086d5f0a6
GuchhaitPratyay <pratyay.guchait@gmail.com>
2021-06-13 10:59:40 +0530

    Commit

--- SearchingAndSorting/TwoPeopleMeet.java
+++ SearchingAndSorting/TwoPeopleMeet.java
@@ -3,21 +3,35 @@ package SearchingAndSorting;
 import java.util.Scanner;
 
 public class TwoPeopleMeet {
-    private static boolean isMeet(int d1 , int v1 , int d2 , int v2) {
 
+    private static boolean isMeet(int distance1 , int velocity1 , int distance2 , int velocity2) {
+        if ((distance1  > distance2 && velocity1 > velocity2) || (distance2 > distance1 && velocity2 >velocity1)) {
+            return false;
+        }
+
+        int relativeDistance = Math.abs(distance1 - distance2);
+        int relativeVelocity = Math.abs(velocity1 - velocity2);
+
+        return relativeDistance % relativeVelocity == 0;
     }
 
     public static void main(String[] pg) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the Position of A: ");
-        int d1 = in.nextInt();
+        int distance1 = in.nextInt();
         System.out.print("Enter the Speed of A: ");
-        int v1 = in.nextInt();
+        int velocity1 = in.nextInt();
 
         System.out.print("Enter the Position of B: ");
-        int d2 = in.nextInt();
+        int distance2 = in.nextInt();
         System.out.print("Enter the Speed of B: ");
-        int v2 = in.nextInt();
+        int velocity2 = in.nextInt();
 
+        if (isMeet(distance1,velocity1,distance2,velocity2)) {
+            System.out.println("They will Meet!!");
+        } else {
+            System.out.println("They will Not Meet!!");
+        }
+        in.close();
     }
 }
