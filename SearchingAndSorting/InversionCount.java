a14d602b8f47a180521d726560f5646a304087b0
GuchhaitPratyay <pratyay.guchait@gmail.com>
2021-06-16 21:47:31 +0530

    Commit

--- SearchingAndSorting/InversionCount.java
+++ SearchingAndSorting/InversionCount.java
@@ -1,5 +1,19 @@
 package SearchingAndSorting;
 
 public class InversionCount {
-    private static int
+    private static int countingInversion(int[] arr) {
+        int count = 0;
+        for (int i = 0 ; i < arr.length ; ++i) {
+            for (int j = 0 ; j < arr.length ; ++j) {
+                if (arr[i] > arr[j]) {
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] pg) {
+
+    }
 }
